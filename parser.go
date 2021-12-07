f133bbd0100023924e4358fc71046a508ffe7b35
Eric Neidhardt <eric.neidhardt@gmail.com>
2021-12-07 12:28:44 +0100

    Added parser
    Parser is responsible for getting alle image declarations
    from either dockerfile or docker-compose.yml.

---
+++ parser.go
@@ -0,0 +1,101 @@
+package gogetdockerimage
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"unicode"
+)
+
+// GetImagesFromDockefile returns all images found in given Dockerfile.
+// It search for occurences of "FROM imageName" or "FROM imageName AS alias"
+// Search is performed case insensitive.
+// Declaration of scrath is ignored.
+func GetImagesFromDockefile(dockerfilePath string) ([]string, error) {
+	lines, err := readFileLines(dockerfilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var images []string
+
+	for _, l := range lines {
+		// search for possible image
+		// expected format is FROM imageName AS alias
+		index := strings.Index(strings.ToUpper(l), "FROM ")
+		// line does not contain imageName
+		if index == -1 {
+			continue
+		}
+		imageString := l[index+len("FROM "):]
+		imageName := ""
+		for _, r := range imageString {
+			// reached end of imageName => stop search
+			if unicode.IsSpace(r) && len(imageName) != 0 {
+				break
+			}
+			// looking for start of imageName
+			if unicode.IsSpace(r) {
+				continue
+			}
+			imageName += string(r)
+		}
+		// if image is not Scratch => append to result
+		if !strings.Contains(strings.ToUpper(imageName), "SCRATCH") {
+			images = append(images, imageName)
+		}
+	}
+	return images, nil
+}
+
+// GetImagesFromDockeCompose returns all images found in given docker-compose file.
+// It search for occurences of "image: <imageName>" or "image: "<imageName>""
+// Search is performed case insensitive.
+func GetImagesFromDockeCompose(dockerfilePath string) ([]string, error) {
+	lines, err := readFileLines(dockerfilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var images []string
+
+	for _, l := range lines {
+		// search for possible image
+		// expected format is image: imageName
+		index := strings.Index(strings.ToUpper(l), "IMAGE: ")
+		// line does not contain imageName
+		if index == -1 {
+			continue
+		}
+		imageString := l[index+len("IMAGE: "):]
+		imageName := ""
+		for _, r := range imageString {
+			// reached end of imageName => stop search
+			if (unicode.IsSpace(r) || r == '"') && len(imageName) != 0 {
+				break
+			}
+			// looking for start of imageName
+			if unicode.IsSpace(r) || r == '"' {
+				continue
+			}
+			imageName += string(r)
+		}
+		images = append(images, imageName)
+	}
+	return images, nil
+}
+
+func readFileLines(filePath string) (lines []string, err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	return
+}
